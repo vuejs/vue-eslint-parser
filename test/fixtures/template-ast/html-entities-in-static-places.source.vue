89b16de68360e87f81c455268ba9b8a0e1dc5b41
Toru Nagashima <star.ctor@gmail.com>
2017-04-22 09:36:32 +0900

    Fix: wrong ranges for HTML entities

---
+++ test/fixtures/template-ast/html-entities-in-static-places.source.vue
@@ -0,0 +1,4 @@
+<template>
+    <!-- a &amp;&amp; b -->
+    <div foo="a &amp;&amp; b">a &amp;&amp; b</div>
+</template>
---
+++ test/fixtures/template-ast/html-entities-with-line-terminators.source.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>{{
+        a
+        &amp;&amp;
+        b
+    }}</div>
+</template>
