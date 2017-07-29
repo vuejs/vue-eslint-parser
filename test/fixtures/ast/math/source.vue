569e8d89bb3b5b3b9aae6682c47cecadbc73b25e
Toru Nagashima <star.ctor@gmail.com>
2017-07-30 02:26:29 +0900

    Chore: add tests about MathML

---
+++ test/fixtures/ast/math/source.vue
@@ -0,0 +1,47 @@
+<template>
+    <math>
+        <mrow>
+            <mi>x</mi>
+            <mo>=</mo>
+            <mfrac>
+                <mrow>
+                    <mrow>
+                        <mo>-</mo>
+                        <mi>b</mi>
+                    </mrow>
+                    <mo>&PlusMinus;</mo>
+                    <msqrt>
+                        <mrow>
+                            <msup>
+                                <mi>b</mi>
+                                <mn>2</mn>
+                            </msup>
+                            <mo>-</mo>
+                            <mrow>
+                                <mn>4</mn>
+                                <mo>&InvisibleTimes;</mo>
+                                <mi>a</mi>
+                                <mo>&InvisibleTimes;</mo>
+                                <mi>c</mi>
+                            </mrow>
+                        </mrow>
+                    </msqrt>
+                </mrow>
+                <mrow>
+                    <mn>2</mn>
+                    <mo>&InvisibleTimes;</mo>
+                    <mi>a</mi>
+                </mrow>
+            </mfrac>
+        </mrow>
+        <annotation encoding="application/x-tex">
+            x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}
+        </annotation>
+        <annotation-xml encoding="application/xhtml+xml">
+            <div>Annotation</div>
+        </annotation-xml>
+        <annotation-xml>
+            <svg><path d=""/></svg>
+        </annotation-xml>
+    </math>
+</template>
