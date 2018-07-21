adbaf4b7a4651d0b3c7627e9aba74adf9c7c3d8c
Toru Nagashima <star.ctor@gmail.com>
2018-07-21 15:42:03 +0900

    Update: HTML tag names to following WHATWG spec

--- src/html/util/tag-names.ts
+++ src/html/util/tag-names.ts
@@ -8,24 +8,20 @@
  * HTML tag names.
  */
 export const HTML_TAGS = new Set([
-    "a", "abbr", "acronym", "address", "applet", "area", "article", "aside", 
-    "audio", "b", "base", "basefont", "bdi", "bdo", "bgsound", "big", "blink", 
-    "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", 
-    "code", "col", "colgroup", "command", "content", "data", "datalist", "dd", 
-    "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "element", 
-    "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", 
-    "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", 
-    "hgroup", "hr", "html", "i", "iframe", "image", "img", "input", "ins", 
-    "isindex", "kbd", "keygen", "label", "legend", "li", "link", "listing", 
-    "main", "map", "mark", "marquee", "math", "menu", "menuitem", "meta", 
-    "meter", "multicol", "nav", "nextid", "nobr", "noembed", "noframes", 
-    "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", 
-    "picture", "plaintext", "pre", "progress", "q", "rb", "rbc", "rp", "rt", 
-    "rtc", "ruby", "s", "samp", "script", "section", "select", "shadow", "slot",
-    "small", "source", "spacer", "span", "strike", "strong", "style", "sub", 
-    "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", 
-    "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", 
-    "var", "video", "wbr", "xmp",
+    "a", "abbr", "address", "area", "article","aside", "audio", "b", "base",
+    "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption",
+    "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del",
+    "details", "dfn", "dialog", "div", "dl", "document", "dt", "em", "embed",
+    "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3",
+    "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe",
+    "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main",
+    "map", "mark", "marquee", "menu", "meta", "meter", "nav", "noscript",
+    "object", "ol", "optgroup", "option", "output", "p", "param", "picture",
+    "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script",
+    "section", "select", "slot", "small", "source", "span", "strong", "style",
+    "sub", "summary", "sup", "table", "tbody", "td", "template", "textarea",
+    "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "var",
+    "video", "wbr"
 ])
 
 /**
