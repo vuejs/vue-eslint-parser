068b1b94f6d53b740d9c46ba48b45d9a18bff7a5
yosuke ota <otameshiyo23@gmail.com>
2021-07-17 17:19:16 +0900

    update testcase

--- test/fixtures/document-fragment/style-variables05/source.vue
+++ test/fixtures/document-fragment/style-variables05/source.vue
@@ -1,8 +1,8 @@
 <style>
   .text{
     color: v-bind(  color  );
-    font-size: v-bind(  'font.size'  );
-    border-color: v-bind(  "border ( 'color' ) "  );;
+    font-size: v-bind(  '  font.size  '  );
+    border-color: v-bind(  "  border ( 'color' ) "  );;
     background-color: v-bind(  
         background
         .
