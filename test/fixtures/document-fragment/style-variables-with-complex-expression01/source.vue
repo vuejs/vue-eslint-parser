f7abdd8e2c587c258a6fd54d620ed2dbefd20152
ota-meshi <otameshiyo23@gmail.com>
2022-01-22 02:37:30 +0900

    Support complex expression in CSS `v-bind()`

---
+++ test/fixtures/document-fragment/style-variables-with-complex-expression01/source.vue
@@ -0,0 +1,11 @@
+<style>
+div {
+  color: v-bind((a + b) / 2 + 'px' );
+}
+div {
+  color: v-bind    ((a + b) / 2 + 'px' );
+}
+p {
+  color: v-bind(((a + b)) / (2 * a));
+}
+</style>
---
+++ test/fixtures/document-fragment/style-variables-with-complex-expression02/source.vue
@@ -0,0 +1,9 @@
+<style>
+div {
+  width: calc(v-bind((a + b) / 2 + 'px' ) - 3px);
+}
+p {
+  /* missing close */
+  width: v-bind(((a + b)) / (2 * a)
+}
+</style>
