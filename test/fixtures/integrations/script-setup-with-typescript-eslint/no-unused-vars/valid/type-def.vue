8c27cb3c989371fde44c0a4fb3a740f8fc294ef8
yosuke ota <otameshiyo23@gmail.com>
2023-04-30 16:00:27 +0900

    Fix false positives for type def with script setup in ts no-unused-vars

---
+++ test/fixtures/integrations/script-setup-with-typescript-eslint/no-unused-vars/valid/type-def.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MyComponent from "./components/MyComponent.vue";
+interface Foo {
+  foo?: string;
+}
+type Bar = string;
+</script>
+
+<template>
+  <MyComponent
+    :foo="{} as Foo"
+    :bar="'s' as Bar"
+  />
+</template>
