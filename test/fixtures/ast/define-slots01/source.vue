8e8aa969eb94968a05ba8c2638ffa260facdb0b5
Yosuke Ota <otameshiyo23@gmail.com>
2023-05-14 09:46:04 +0900

    Add support for defineOptions and defineSlots (#193)

---
+++ test/fixtures/ast/define-slots01/source.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+const slots = defineSlots<{
+  default(props: { msg: string }): any
+}>()
+</script>
---
+++ test/fixtures/ast/define-slots02-without-ts/source.vue
@@ -0,0 +1,3 @@
+<script setup>
+const slots = defineSlots()
+</script>
