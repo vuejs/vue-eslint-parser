61f1a4d1633c1e691e8bc1f3c6f8a6d37f6de6df
ota-meshi <otameshiyo23@gmail.com>
2024-01-23 08:20:20 +0900

    Fix an error when using default for generic

---
+++ test/fixtures/ast/vue3.3-generic-6-with-default/source.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts" generic="
+  T // Comments
+    extends /* = */ Foo = number,
+    // Comments
+  U /* extends */
+    extends
+    Record&lt;
+      /* = */
+      string, // =
+      T
+    >
+     = Record<string, number&gt;
+">
+type Foo = number | string
+const p = defineProps<{foo:T, bar: U}>()
+const foo = p.foo
+console.log(foo)
+</script>
+<template>
+{{foo}}
+</template>
