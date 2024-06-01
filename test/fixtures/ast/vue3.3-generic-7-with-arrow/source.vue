c7b5fbf8032b936a575a309e87a3d69b9288b758
Yosuke Ota <otameshiyo23@gmail.com>
2024-06-01 10:09:25 +0900

    Fixed parsing error when arrow function type in generics (#234)
    * Fixed parsing error when arrow function type in generics
    
    * macos-latest to macos-12

---
+++ test/fixtures/ast/vue3.3-generic-7-with-arrow/source.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts" generic="
+  T extends () => string,
+  U  extends () => string = () => 'abc'
+">
+const p = defineProps<{t: T, u: U}>()
+</script>
+
+<template>
+{{p.t()}}
+{{p.u()}}
+</template>
