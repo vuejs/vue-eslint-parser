1cdfa2f9ee5d8367e6deb7eb5d47a3a62e7c1f70
Vida Xie <vida_2020@163.com>
2025-07-05 00:13:02 +0800

    feat: move `crlf` to `vitest`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig} from 'vitest/config'
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-  test: {
-    include: ['test/parser-options.test.ts']
-  }
+    test: {
+        include: ["test/{parser-options,crlf}.test.ts"],
+    },
 })
