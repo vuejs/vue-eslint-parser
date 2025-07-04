1ec01140e707a79d86b82cb0266e80de352f9338
Vida Xie <vida_2020@163.com>
2025-07-04 22:27:55 +0800

    chore: update

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "tsdown"
 
+const banner = `/**
+ * @author Toru Nagashima <https://github.com/mysticatea>
+ * See LICENSE file in root directory for full license.
+ */`
+
 export default defineConfig({
     entry: "./src/index.ts",
     target: "es2015",
     sourcemap: true,
     outputOptions: {
-        banner: `/**
- * @author Toru Nagashima <https://github.com/mysticatea>
- * See LICENSE file in root directory for full license.
- */`,
+        banner,
     },
     dts: true,
     format: "cjs",
