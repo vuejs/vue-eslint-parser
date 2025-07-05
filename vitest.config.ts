4f1c036e6cd0c99f10acd4fe10a6b258df5783b3
Vida Xie <vida_2020@163.com>
2025-07-05 13:10:04 +0800

    feat: move `define-custom-blocks-visitor` to `vitest`

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
     test: {
-        include: ["test/{parser-options,crlf,define-document-visitor}.test.ts"],
+        include: [
+            "test/{parser-options,crlf,define-document-visitor,define-custom-blocks-visitor}.test.ts",
+        ],
     },
 })
